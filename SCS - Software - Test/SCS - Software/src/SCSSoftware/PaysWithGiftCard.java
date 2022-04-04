82348128a996530341981dcac44914bfc02628f4
PKDT-93 <75346912+PKDT-93@users.noreply.github.com>
2022-04-04 16:49:13 -0600

    Created new classes
    Co-Authored-By: Peter <44074396+peter-kuchel@users.noreply.github.com>

--- SCS - Software - Test/SCS - Software/src/SCSSoftware/PaysWithGiftCard.java
+++ SCS - Software - Test/SCS - Software/src/SCSSoftware/PaysWithGiftCard.java
@@ -1,6 +1,46 @@
 package SCSSoftware;
 
-public class PaysWithGiftCard 
+import org.lsmr.selfcheckout.devices.CardReader;
+import org.lsmr.selfcheckout.Card.CardData;
+import org.lsmr.selfcheckout.Card.CardSwipeData;
+
+public class PaysWithGiftCard implements CardReaderObserver
 {
+	
+	private GiftCardDatabase giftcarddatabase;
+	private String getnumber;
+	private String getpin;
+	private Checkout checkout;
+	private BigDecimal transactionAmount;
+	
+	public void cardInserted(CardReader reader) {
+		// IGNORE
+	}
+
+	public void cardRemoved(CardReader reader) {
+		// IGNORE
+	}
+
+	public void cardTapped(CardReader reader) {
+		// IGNORE
+	}
+
+	public void cardSwiped(CardReader reader) {
+		// IGNORE
+	}
+	
+	/* This method gathers customer information from the card reader and assigns it to local attributes*/
+
+	public void cardDataRead(CardReader reader, CardData data) {
+		
+	}
+	
+	
+	public PaysWithCard(GiftCardDatabase giftcardDatabase, Checkout checkout)
+	{
+		this.giftcarddatabase = giftcardDatabase;
+		
+	}
+	
 	
 }
