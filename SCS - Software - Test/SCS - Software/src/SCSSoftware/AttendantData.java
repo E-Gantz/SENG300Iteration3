fcfd98f56746791445c96342e06e37e911c6a7a4
Sjnaka <wu.dingkai@hotmail.com>
2022-04-09 15:42:40 -0600

    updated from Rachel
    Co-Authored-By: RachelRalph <60715884+RachelRalph@users.noreply.github.com>

--- SCS - Software - Test/SCS - Software/src/SCSSoftware/AttendantData.java
+++ SCS - Software - Test/SCS - Software/src/SCSSoftware/AttendantData.java
@@ -4,42 +4,44 @@ import java.util.*;
 import java.util.stream.Collectors;
 
 public class AttendantData {
-    Map<String, String> userMap = new HashMap<String, String>();
+    public Map<String, String> userMap = new HashMap<String, String>();
 
     Scanner keyboard = new Scanner(System.in);
-    String username, password;
+    private String username;
 
     public void addAttendant(String username, String password) {
         userMap.put(username, password);
     }
 
-    public String removeAttendant(String username, String password) {
+    public boolean removeAttendant(String username) {
         if (userMap.containsKey(username)) {
-            String dbPassword = userMap.get(username);
-            if (dbPassword.equals(password)) {
                 userMap.remove(username);
-                return "Attendant removed from the database";
-            } else {
-                return "Password does not match";
-            }
-        } else {
-            return "Username not found";
+                return true;
         }
+        return false;
     }
 
-    // public boolean validateUsername(String name) {
-    //     if (!userMap.containsKey(username)) {
-    //         System.out.println("Incorrect user name");
-    //         System.exit(0);
-    //     }
+    public boolean logIn(String username, String password){
+        if (!userMap.containsKey(username)) return false;
+        if (!userMap.get(username).equals(password)) return false;
+        if(this.username != null) return false;
 
-    //     User un = userMap.get(username);
-    // }
+        this.username = username;
+        return true;
 
-    // public boolean validatePassword() {
-    //     if ((un.password).equals(password)) {
-    //         System.out.println("Correct password");
-    //     } else
-    //         System.out.println("Incorrect password");
-    // }
+    }
+
+    public void logOut(){
+        this.username = null;
+    }
+
+    public String getCurrentUser(){
+        return username;
+    }
+
+    public Map getUsers(){
+        return userMap;
+    }
+
+
 }
