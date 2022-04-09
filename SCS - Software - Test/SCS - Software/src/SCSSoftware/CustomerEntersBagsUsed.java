427de62c25e8fd6561eea0601b2b174da7412024
carlo-deg <dg.carlodg@gmail.com>
2022-04-09 14:59:40 -0600

    Update CustomerEntersBagsUsed.java

--- SCS - Software - Test/SCS - Software/src/SCSSoftware/CustomerEntersBagsUsed.java
+++ SCS - Software - Test/SCS - Software/src/SCSSoftware/CustomerEntersBagsUsed.java
@@ -2,35 +2,19 @@ package SCSSoftware;
 
 import org.lsmr.selfcheckout.InvalidArgumentSimulationException;
 
-public class CustomerEntersBagsUsed /*extends CustomerOwnBag*/ {
+public class CustomerEntersBagsUsed {
 	private double totalBags;
 	private boolean purchaseBag;
-	//private boolean additionalBags;
 	
 	public CustomerEntersBagsUsed(double totalBags, boolean purchaseBag) {
-		//super(weightInGrams, bagWeight);
 		if (totalBags < 0.0) {
 			throw new InvalidArgumentSimulationException("Cannot have negative of plastic bags");
 		} else if (totalBags == 0.0){
 			this.purchaseBag = false;
-			//throw new InvalidArgumentSimulationException("Customer has brought own bags. No plastic bags needed");
 		} else {
 			this.totalBags = totalBags;
 			this.purchaseBag = true;
 		}
-		/**
-		 * Different approach I was thinking of for the constructor 
-		 */
-		/*
-		if(bagWeight < 0.0) {
-			throw new InvalidArgumentSimulationException("Error with the scale");
-		} else if(bagWeight == 0) {
-			this.totalBags = totalBags;
-		} else {
-			this.totalBags++;
-		}
-		*/
-		
 	}
 	
 	/**
