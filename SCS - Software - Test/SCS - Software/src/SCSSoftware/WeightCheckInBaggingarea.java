1f8f5d4f04d4418b98c95b0babd999c62b09c092
RachelRalph <60715884+RachelRalph@users.noreply.github.com>
2022-04-10 17:23:12 -0600

    Add files via upload

--- SCS - Software - Test/SCS - Software/src/SCSSoftware/WeightCheckInBaggingarea.java
+++ SCS - Software - Test/SCS - Software/src/SCSSoftware/WeightCheckInBaggingarea.java
@@ -10,19 +10,34 @@ import org.lsmr.selfcheckout.devices.OverloadException;
 // Station detects that the weight in the bagging area does not conform to expectations
 public class WeightCheckInBaggingarea {
 	private ProductCart pcart;
+	private double weightDiscrepency;
 
 	public WeightCheckInBaggingarea(ProductCart pcart) {
+
 		this.pcart = pcart;
+		weightDiscrepency = 0;
 	}
 
 	// check the weights 
 	public boolean isWeightAsExpect(double weights){
 		boolean flag = false;
-		if(Math.abs(weights-this.pcart.getTotalExpectedWeight()) > 0.5){
+		if(Math.abs(weights-(this.pcart.getTotalExpectedWeight() + weightDiscrepency)) < 0.5){
 			flag = true;
 		}
 		return flag;
 	}
+
+	public boolean approveWeight(AttendantData attendantData, double weight){
+		if(attendantData.getCurrentUser() == null) return false;
+		weightDiscrepency = weight - this.pcart.getTotalExpectedWeight();
+		return true;
+	}
+
+	public double getWeightDiscrepency(){
+		return weightDiscrepency;
+	}
+
+
 
 
 
