c3778a3b4fbf73c1aa43c74affc532739ab9efd3
rei-tsunemi <reren1031@gmail.com>
2022-04-09 18:12:15 -0600

    Revert "Merging Rei's branch to Main"
    This reverts commit d12b71741ebaca29520112df041dd6ebe01de1ac.

--- SCS - Software - Test/SCS - Software/src/SCSSoftware/ItemPlacer.java
+++ SCS - Software - Test/SCS - Software/src/SCSSoftware/ItemPlacer.java
@@ -6,7 +6,6 @@ import org.lsmr.selfcheckout.InvalidArgumentSimulationException;
 import org.lsmr.selfcheckout.devices.AbstractDevice;
 import org.lsmr.selfcheckout.devices.BarcodeScanner;
 import org.lsmr.selfcheckout.devices.ElectronicScale;
-import org.lsmr.selfcheckout.devices.OverloadException;
 import org.lsmr.selfcheckout.devices.observers.AbstractDeviceObserver;
 import org.lsmr.selfcheckout.devices.observers.ElectronicScaleObserver;
 
@@ -15,19 +14,22 @@ public class ItemPlacer implements ElectronicScaleObserver {
 	private double previousWeight;
 	private double currentWeight;
 	private ProductCart pcart;
-	private BarcodeScanner scanner;
-	private ItemNotPlaceable notplaceable;
-	private Boolean NotInBags;
+	public BarcodeScanner scanner;
+	public BarcodeScanner handScanner;
+	private Boolean notInBags;
 	private Timer timer;
 	private CustomerOwnBag ownbag;
+	private boolean timerRunning;
 	
-	public ItemPlacer(BarcodeScanner scanner, ProductCart pcart) {
-		this.scanner = scanner;
+	public ItemPlacer(BarcodeScanner mainScanner, ProductCart pcart, BarcodeScanner handScanner) { //need both scanners to enable them after the item is placed.
+		this.scanner = mainScanner;
 		this.pcart = pcart;
+		this.handScanner = handScanner;
 		this.previousWeight = 0.0;
 		this.currentWeight = 0.0;
-		this.NotInBags = false;
+		this.notInBags = false;
 		this.timer = new Timer();
+		this.timerRunning = false;
 	}
 	
 
@@ -47,25 +49,20 @@ public class ItemPlacer implements ElectronicScaleObserver {
 	@Override
 	public void weightChanged(ElectronicScale scale, double weightInGrams) throws InvalidArgumentSimulationException {
 		beforePlacing();
-		notplaceable = new ItemNotPlaceable();
-		expectedWeight = pcart.getCart().get((pcart.getCart().size())-1).getExpectedWeight();//this gets the weight of the item most recently added to the cart.
-		notplaceable.CheckIfPlacable(scale, expectedWeight);
-		if(notplaceable.isPlaceable()) {
-			if(ownbag.checkOwnBag() == false)
-				currentWeight = weightInGrams;
-			else
-				currentWeight = weightInGrams  - ownbag.getBagWeight();
-			if(currentWeight == previousWeight + expectedWeight) {
-				this.previousWeight = currentWeight;
-				this.expectedWeight = 0.0;
-				this.scanner.enable();
-				this.NotInBags = false;
-			}
-			else 
-				throw new InvalidArgumentSimulationException("Wrong item placed on scale!");
-		} else {
-			// Attendant approval required to enable + continue checkout
-//			throw new InvalidArgumentSimulationException("Talk to attendatnt to continue");
+		expectedWeight = pcart.getNewestExpectedWeight();//this gets the weight of the item most recently added to the cart.
+		if(ownbag.checkOwnBag() == false)
+			currentWeight = weightInGrams;
+		else
+			currentWeight = weightInGrams  - ownbag.getBagWeight();
+		if(Math.abs(currentWeight - (previousWeight + expectedWeight)) < 1.5) {
+			this.previousWeight = currentWeight;
+			this.expectedWeight = 0.0;
+			this.scanner.enable();
+			this.handScanner.enable();
+			this.notInBags = false;
+		}
+		else {
+			throw new InvalidArgumentSimulationException("Wrong item placed on scale!");
 		}
 	}
 
@@ -73,27 +70,26 @@ public class ItemPlacer implements ElectronicScaleObserver {
 	public void overload(ElectronicScale scale) {
 		//Auto-generated method stub
 		//put "too heavy!" message on screen or something
-		// temporary locks scanner to allow customer to call attendant and fix the issue
-		this.scanner.disable();
-		throw new InvalidArgumentSimulationException("Item too heavy!");
+		throw new InvalidArgumentSimulationException("Too Heavy!");
 	}
 
 	@Override
 	public void outOfOverload(ElectronicScale scale) {
 		//Auto-generated method stub
 		//remove the "too heavy!" message
-		//attendant somehow fixes the problem
-		notplaceable.setPlaceable(true);
-		this.scanner.enable();
 	}
 	
 	public void startTimer() {
-		BaggingTimeout timeout = new BaggingTimeout(pcart, this);
-		timer.schedule(timeout,50, 500); //this should run the BaggingTimeout run() method every .5 seconds.
+		if (!timerRunning) {
+			timerRunning = true;
+			BaggingTimeout timeout = new BaggingTimeout(pcart, this);
+			timer.schedule(timeout,50, 500); //this should run the BaggingTimeout run() method every .5 seconds.
+		}
+		
 	}
 	
 	public void beforePlacing() {
-		ItemPlacer itmp = new ItemPlacer(scanner, pcart);
+		ItemPlacer itmp = new ItemPlacer(scanner, pcart, handScanner);
 		this.ownbag = new CustomerOwnBag(1.0, itmp.getBagWeight());
 	}
 	
@@ -102,11 +98,23 @@ public class ItemPlacer implements ElectronicScaleObserver {
 	}
 		
 	public void BagTimeout() {
-		NotInBags = true;
+		notInBags = true;
+	}
+	
+	public void timerDone() {
+		timerRunning = false;
+		if (notInBags) {
+			throw new InvalidArgumentSimulationException("Please place your item on the scale.");
+		}
 	}
 	
 	public Boolean getTimeoutStatus() {
-		return this.NotInBags;
+		return this.notInBags;
+	}
+	
+	public void disableScanners() {
+		scanner.disable();
+		handScanner.disable();
 	}
 
 }
