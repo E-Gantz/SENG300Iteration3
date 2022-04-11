1406bac3d5682ff7da92f3b5b63efdf713065bec
PKDT-93 <75346912+PKDT-93@users.noreply.github.com>
2022-04-11 14:01:50 -0600

    java doc commenting

--- SCS - Software - Test/SCS - Software/src/SCSSoftware/MemberCard.java
+++ SCS - Software - Test/SCS - Software/src/SCSSoftware/MemberCard.java
@@ -1,12 +1,21 @@
 package SCSSoftware;
 
 import org.lsmr.selfcheckout.Card;
-
+/**
+ * Represents a MemberCard object which is based on the card class and only contains a card number value and the
+ * cardholder name as a String as arguments into the card class itself and all other arguments are null otherwise
+ */
 public class MemberCard {
 	private Card card;
 	private int points;
 	private String cardNum;
-	
+
+	/**
+	 * Constructs the MemberCard class
+	 *
+	 * @param number
+	 * @param cardholder
+	 */
 	public MemberCard(String number, String cardholder) {
 		this.card = new Card("Member", number, cardholder, null, null, false, false); //create a new card with type Member, null cvv, null pin, tap not enabled, no chip
 		this.cardNum = number;
